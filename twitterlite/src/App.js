94e0b8b5b2fd2a715122ca834e8608e064a1ae07
T-Dex <dex.m.mills@gmail.com>
2018-07-18 04:48:41 -0600

    Test branch

--- twitterlite/src/App.js
+++ twitterlite/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import firebase from './firebase/firebase';
 import './styles/app.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import UserList from './components/userlist'
+import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom'
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 import UserMess from './components/usermess'
 import uuid from 'uuid'
 
@@ -19,24 +21,6 @@ class App extends Component {
     })
   }
 
-  displayUser(userData){
-    const user=userData.checkedUser;
-    const checked=userData.checked;
-    
-    
-    const userchecked= Object.keys(this.state.tweet).map(key=>{
-    let activeUser =this.state.tweet[key]
-    
-      
-      const userID=key
-      const active=activeUser.active
-      console.log(active);
-      refDb.child('user/' + userID+"/active").set(active)
-      
-      
-    })
-  
-  }
   changeUserMessage(newMessage){
     const user= Object.keys(this.state.tweet).map(key=>{
     let activeUser =this.state.tweet[key]
@@ -64,20 +48,27 @@ class App extends Component {
   }
   render() {
 const mappedState=Object.keys(this.state.tweet).map(key=>this.state.tweet[key])
-
+const userName=Object.keys(this.state.tweet).map(key=>{ 
+            return <li><Link to={this.state.tweet[key].userName}>{this.state.tweet[key].userName}</Link></li>
+        })
+  
 
     return (
       <Router>
       <div className="App">
         <header className="App-header"/>
         <div className="main"> 
-          <UserList users={mappedState} displayUser={this.displayUser.bind(this)} />
-          <UserMess tweet={this.state.tweet} changeUserMessage={this.changeUserMessage.bind(this)}/>
+             <div className="User">
+              <li><Link to='/'>Home</Link></li>
+              {userName}
+            </div>
         </div>
+        
       </div>
       </Router>
     );
   }
 }
+
 
 export default App;
--- twitterlite/src/components/userlist.js
+++ twitterlite/src/components/userlist.js
@@ -20,21 +20,21 @@ class UserList extends Component{
        //change checked of user to !checked
     }
     render(){
-        const userName=Object.keys(this.props.users).map(key=>{ 
-            return <FormControlLabel
-            key={key}
-            control={
-                <Switch
-                key={key}
-                checked={this.props.users[key].active}
-                onChange={this.handleChange}
-                component={Link}
-                to={this.props.users[key].userName}
-                value={this.props.users[key].userName}
-                />
-            }label={this.props.users[key].userName} />
-        })
-       
+        // const userName=Object.keys(this.props.users).map(key=>{ 
+        //     return <FormControlLabel
+        //     key={key}
+        //     control={
+        //         <Switch
+        //         key={key}
+        //         checked={this.props.users[key].active}
+        //         onChange={this.handleChange}
+        //         component={Link}
+        //         to={this.props.users[key].userName}
+        //         value={this.props.users[key].userName}
+        //         />
+        //     }label={this.props.users[key].userName} />
+        // })
+       const userName="pedro"
         
         return(
             <div className="User">
